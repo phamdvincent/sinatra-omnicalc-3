cb20cab2d8dabd59fcdefe7a11ceaaabd75a0154
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-02 20:28:21 +0000

    Added content to umbrella portion

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  redirect "/umbrella"
+
+end
+
+get("/umbrella") do
+  erb(:umbrella_form)
+end
+
+post("/process_umbrella") do
+  @user_location = params.fetch("user_location")
+
+  gmaps_key = ENV.fetch("GMAPS_KEY") # Set gmaps key to variable
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{gmaps_key}" # url of google geocode
+
+  @raw_response = HTTP.get(gmaps_url).to_s
+  erb(:umbrella_results)
 end
